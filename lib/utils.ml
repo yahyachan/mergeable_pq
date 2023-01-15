3ff34bce5e920b9d33c6cbaee4dfaaaba0d21cb7
yahyachan <danihao123@outlook.com>
2023-01-15 11:05:19 +0800

    add totally ordered signature

---
+++ lib/utils.ml
@@ -0,0 +1,10 @@
+module type Ord = sig
+  type t
+
+  val compare : t -> t -> int
+  (** [compare a b] returns [0] if [x] is equal to
+      [y], a negative integer if [x] is less
+      than [y], and a positive integer is [x] is
+      greater than [y]. *)
+end
+(** Totally ordered relations. *)
